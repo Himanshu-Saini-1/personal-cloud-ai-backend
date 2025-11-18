97fb6f06cf75cec45c07efd7d408e7c3002d1b2a
HIMANSHUDCRUST <117287565+HIMANSHUDCRUST@users.noreply.github.com>
2025-11-18 17:25:32 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://personal-cloud-frontend.onrender.com",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
